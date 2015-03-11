8013bed93b145aaf561c828996f3b5341fef1e1a
Wes Gilleland <wes.gilleland@gmail.com>
2015-03-11 15:34:23 -0400

    Started adding a new function to separate code

--- Lab 2/Problem 1/main.asm
+++ Lab 2/Problem 1/main.asm
@@ -7,6 +7,8 @@ TITLE MASM Template						(main.asm)
 INCLUDE Irvine32.inc
 .data
 myMessage BYTE "MASM program example",0dh,0ah,0
+promptMessage BYTE "Enter an integer score between 0 and 100 (-1 to exit): ", 0
+
 
 .code
 main PROC
@@ -15,7 +17,15 @@ main PROC
 	mov	 edx,OFFSET myMessage
 	call WriteString
 
+	call func1
 	exit
 main ENDP
+
+func1 PROC
+	call Clrscr
+	; Call the prompt
+	mov edx, OFFSET promptMessage
+	call WriteString
+func1 ENDP
 
 END main
