faa2e99ed7c8b822960632f03192c7ef07f3129d
Wes Gilleland <wes.gilleland@gmail.com>
2015-03-15 23:51:13 -0400

    Done. Bug in Problem 2 while summing elements

--- Lab 2/Problem 1/main.asm
+++ Lab 2/Problem 1/main.asm
@@ -1,31 +1,132 @@
 TITLE MASM Template						(main.asm)
 
-; Description:
-; 
-; Revision date:
+; Assignment: Lab 2 - Problem 1
+
+; Class: CSC 360
+
+; Author: Wes Gilleland
+
+; Date: March 13th, 2015
+
+; Description: This program will ask the user to enter an integer test score between
+; 0 and 100. It will display the appropriate letter grade as well. It will do so in
+; an infinite loop that will break only when the user enters a negative number (but
+; they are prompted for a -1)
+
 
 INCLUDE Irvine32.inc
 .data
-myMessage BYTE "MASM program example",0dh,0ah,0
 promptMessage BYTE "Enter an integer score between 0 and 100 (-1 to exit): ", 0
+
+; These are the messages that will be displayed when the grade is calculated
+gradeAMessage BYTE "Congrats! You got an A!", 0
+gradeBMessage BYTE "B! Not as good as an A, but damn good!", 0
+gradeCMessage BYTE "C! Hey, how does it feel to be average, not B average?", 0
+gradeDMessage BYTE "D! At least D's get degrees!", 0
+gradeFMessage BYTE "F! Maybe this school thing isn't for you", 0
+
+showGradeMessage BYTE "The letter grade is: ", 0
+
+gradeInt SDWORD 0				; The variable to store the numeric value of the grade
 
 
 .code
 main PROC
-	call Clrscr
 
-	mov	 edx,OFFSET myMessage
-	call WriteString
+	call Clrscr					; Clear the screen
 
-	call func1
+	runLoop:					; Who says infinite loops are bad?
+		call displayPrompt		; Call the prompt
+		call getInput			; Get the input
+		call handleInput		; Handle it. Here is where we'll do our checks for 
+		call Crlf				; New line!
+		jmp runLoop				; Let's loop again!
+		
 	exit
 main ENDP
 
-func1 PROC
-	call Clrscr
-	; Call the prompt
+displayPrompt PROC
+; Just displays the prompt
 	mov edx, OFFSET promptMessage
 	call WriteString
-func1 ENDP
+
+	ret
+; POST: edx contains the offset of promptMessage
+displayPrompt ENDP
+
+getInput PROC
+; Just gets the input from the user
+	call ReadInt				; Call the procedure to get the integer
+	mov gradeInt, eax			; Move the input into the grade
+
+	ret
+; POST: eax now contains the user input
+getInput ENDP
+
+handleInput PROC
+; This will determine what kind of grade the user got and display the appropriate message
+; PRE: gradeInt should be set to some numeric value
+	cmp gradeInt, 90			
+	jge printAGrade				; If >= 90, they have an A
+
+	cmp gradeInt, 80			
+	jge printBGrade				; If >= 80, but < 90, they have a B
+
+	cmp gradeInt, 70
+	jge printCGrade				; If >= 70, but < 80, they have a C
+
+	cmp gradeInt, 60
+	jge printDGrade				; If >= 60, but < 70, they have a D
+
+	cmp gradeInt, 0
+	jge printFGrade				; If >= 0, but < 60, they have an F
+
+	exit						; The user is entering a negative number, so exit the program
+
+	printAGrade:
+		; Print the message showing they got an A
+		mov edx, OFFSET showGradeMessage
+		call WriteString
+		mov edx, OFFSET gradeAMessage
+		call WriteString
+		jmp returnBlock			; Jump to the return statement
+
+	printBGrade:
+		; Print the message showing they got a B
+		mov edx, OFFSET showGradeMessage
+		call WriteString
+		mov edx, OFFSET gradeBMessage
+		call WriteString
+		jmp returnBlock			; Jump to the return statement
+
+	printCGrade:
+		; Print the message showing they got a C
+		mov edx, OFFSET showGradeMessage
+		call WriteString
+		mov edx, OFFSET gradeCMessage
+		call WriteString
+		jmp returnBlock			; Jump to the return statement
+
+	printDGrade:
+		; Print the message showing they got a D
+		mov edx, OFFSET showGradeMessage
+		call WriteString
+		mov edx, OFFSET gradeDMessage
+		call WriteString
+		jmp returnBlock			; Jump to the return statement
+
+	printFGrade:
+		; Print the message showing they got an F
+		mov edx, OFFSET showGradeMessage
+		call WriteString
+		mov edx, OFFSET gradeFMessage
+		call WriteString
+		jmp returnBlock			; Jump to the return statement
+
+	returnBlock:
+		; The return statement
+		ret
+; POST: edx contains the offset of the appropriate message variable
+handleInput ENDP
 
 END main
--- Lab 2/Problem 2/main.asm
+++ Lab 2/Problem 2/main.asm
@@ -1,21 +1,109 @@
 TITLE MASM Template						(main.asm)
 
-; Description:
-; 
-; Revision date:
+; Author: Wes Gilleland
+
+; Class: CSC 360
+
+; Date: March 13th, 2015
+
+; Description: This program will ask the user for a positive integer to define the size of an array. It is
+; very specific about what it wants, an intger between 0 and 20, inclusive. It will ask generically at first,
+; then if the first input is not what is expected, will clarify and continue asking until the input is valid.
+; It will ask the user n number of times for an integer to store into the array. After that it will sum the elements
+; in the array and display the sum. Currently there is a bug where it does not fetch the individual values in the array
+; but fetches the entire array.
 
 INCLUDE Irvine32.inc
 .data
-myMessage BYTE "MASM program example",0dh,0ah,0
+prompt1 	BYTE "Enter an integer: ", 0
+sumPrompt1 	BYTE "The sum is: ", 0
+prompt2 	BYTE "Please enter a positive number that fits the following criteria: 0 < n <= 20: ", 0
+theArray	SDWORD 20 DUP(?)								; Initialize an array with a max length of 20 4 byte signed integers
+arraySize 	SDWORD ?										; Initialize the array size with an unknown value of signed double word
 
 .code
 main PROC
+
 	call Clrscr
-
-	mov	 edx,OFFSET myMessage
+	; Get the input
+	call getArraySizeFromUser
+	; Get the integers for the array
+	call getArrayValues
+	; Print out the result
+	call sumArray
+	mov edx, OFFSET sumPrompt1
 	call WriteString
-
+	call WriteInt
+	call Crlf
+	call WaitMsg							; Wait for the user to exit the program
 	exit
 main ENDP
+
+getArraySizeFromUser PROC
+; PRE: Doesn't really modify anything. Messes with EAX
+	; Ask nicely the first time
+	mov edx, OFFSET prompt1
+	call WriteString						; Display the first prompt: "Enter an integer"
+	call ReadInt							; Read the input, storing it in eax
+	mov arraySize, eax						; Store the value
+	; Check to see if within acceptable range
+	; If so, move to return
+	cmp arraySize, 20 						; Check that we're within the upper limit
+	jg 	askAgain
+	cmp arraySize, 0						; Check that we're within the lower limit
+	jle	askAgain
+	jmp returnOut1							; Everything's good, jump to the return
+
+	askAgain:								; The user didn't enter a valid input. Ask again but clarify this time
+		call Crlf							; Clear the screen
+		mov edx, OFFSET prompt2				; Display the prompt
+		call WriteString					; Display the prompt
+		call ReadInt						; Read input
+		mov arraySize, eax					; Store the input
+
+		; Check to see if within acceptable range
+		; If so, move to return
+		cmp arraySize, 20 						; Check that we're within the upper limit
+		jg 	askAgain
+		cmp arraySize, 0						; Check that we're within the lower limit
+		jle	askAgain
+
+	returnOut1:
+		ret
+; POST: EAX now contains the value input by the user, as does arraySize
+getArraySizeFromUser ENDP
+
+getArrayValues PROC
+; PRE: reset esi and move the value from arraySize into ecx
+	; set up the loop
+	mov esi, 0								; Set up the index
+	mov ecx, arraySize 						; We're going to be looping arraySize times
+	tehLoop:
+		call Crlf 							; Run the 
+		mov edx, OFFSET prompt1
+		call WriteString					; Display the prompt
+		call ReadInt
+		mov theArray[esi], eax				; Move the value into the array at the current index
+		inc esi								; Increment the index
+		loop tehLoop
+
+	ret
+; POST: theArray now has a number of N integers contained in it, where N is arraySize
+getArrayValues ENDP
+
+sumArray PROC
+; BUG: Doesn't grab individual values from the array, but rather a whole bunch of them at once. Don't know why...
+	; set up the loop
+	mov ecx, LENGTHOF theArray 						; We're looping arraySize times
+	mov ebx, OFFSET theArray				; Get the address of the first array element
+	mov eax, 0
+	top:
+		add eax, [ebx]						; Get the value and add it to eax
+		add edx, TYPE theArray				; Increment the pointer to the next value
+		loop top 							; Here we go loop de loop!
+
+	ret
+; POST: EAX should contain the sum of all elements of theArray
+sumArray ENDP
 
 END main
